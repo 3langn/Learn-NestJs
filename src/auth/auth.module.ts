0d9942e4215ef9cd9437be9370803cdb6d2700bf
MinhNguyen <3langn@gmai.com>
2021-11-23 00:16:38 +0700

    passport jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,25 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRATION_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,30 @@
 import {
   BadRequestException,
   Injectable,
-  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcryptjs';
-import { UserRegisterDto } from 'src/users/dto/user-register.dto';
 import { UserLoginDto } from 'src/users/dto/user-login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly JwtService: JwtService,
+  ) {}
 
   async validateUser(userLoginDto: UserLoginDto) {
-    const user = await this.userService.findByEmail(userLoginDto.email);
-
-    const isMatchPassword = bcrypt.compareSync(
-      userLoginDto.password,
-      user.password,
+    const { password, ...rest } = await this.userService.findByEmail(
+      userLoginDto.email,
     );
+
+    const isMatchPassword = bcrypt.compareSync(userLoginDto.password, password);
 
     if (!isMatchPassword) {
       throw new UnauthorizedException('Password is incorrect');
     }
-    return user;
+    return this.JwtService.sign(rest);
   }
 }
